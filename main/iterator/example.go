9aecb3f7734fe14bd19f8e670d7f78326a7c6bea
aesperer <piyrw9754@gmail.com>
2023-06-16 23:21:00 +0900

    object range loop

--- main/iterator/example.go
+++ main/iterator/example.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type Obj struct {
+	Name string
+	Age  int
+}
+
+func PrintObject(list []Obj) {
+	for index, object := range list {
+		fmt.Printf("index: %d, object: %+v\n", index, object)
+	}
+}
+
 func main() {
 
 	for index := 1; index <= 10; index++ {
@@ -43,5 +54,26 @@ func main() {
 	for key, value := range dictionary {
 		fmt.Printf("key: %s, value: %s\n", key, value)
 	}
+
+	list := []Obj{
+		{"Beckham", 11},
+		{"Zidane", 7},
+		{"Ronaldo", 9},
+	}
+
+	for _, object := range list {
+		object.Age = object.Age * 2
+	}
+
+	// 출력, 11, 7, 9
+	PrintObject(list)
+
+	for index := range list {
+		object := &list[index]
+		object.Age = object.Age * 2
+	}
+
+	// 출력, 22, 14, 18
+	PrintObject(list)
 
 }
