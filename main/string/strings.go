17152a6a5c69fda40e71455f1b01168f7e789cfc
aesperer <piyrw9754@gmail.com>
2023-06-16 22:35:19 +0900

    문자열 대치

---
+++ main/string/strings.go
@@ -0,0 +1,18 @@
+package string
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	// 문자열 합치기 Join
+	strs := []string{"a", "b", "c"}
+	fmt.Println(strings.Join(strs, ":")) // a:b:c
+
+	// 문자열 대치 (replace)
+	str := "a.b.c"
+	r := strings.Replace(str, ".", "_", -1)
+	fmt.Println(r) // a_b_c
+}
